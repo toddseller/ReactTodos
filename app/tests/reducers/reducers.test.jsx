417e27694ab98338d5247ea073aff4d0f7662526
Todd Seller <me@toddseller.com>
2016-07-02 16:13:27 -0700

    Update todos on firebase

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -66,8 +66,8 @@ describe('Reducers', () => {
       })
     })
 
-    describe('toggleTodo', () => {
-      it('should toggle todo', () => {
+    describe('startToggleTodo', () => {
+      it('should update todo', () => {
         var todos = [{
             id: 1,
             text: 'Walk the dog',
@@ -75,14 +75,20 @@ describe('Reducers', () => {
             createdAt: 123,
             completedAt: 125
         }]
+        var updates = {
+          completed: false,
+          completedAt: null
+        }
         var action = {
-          type: 'TOGGLE_TODO',
-          id: 1
+          type: 'UPDATE_TODO',
+          id: todos[0].id,
+          updates
         }
         var res = reducers.todosReducer(df(todos), df(action))
 
-        expect(res[0].completed).toEqual(false)
-        expect(res[0].completedAt).toEqual(undefined)
+        expect(res[0].completed).toEqual(updates.completed)
+        expect(res[0].completedAt).toEqual(updates.completedAt)
+        expect(res[0].text).toEqual(todos[0].text)
       })
     })
   })
