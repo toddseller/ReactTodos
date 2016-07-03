d8a5540f00d233f12c7f71a0fc0c5af263668d86
Todd Seller <me@toddseller.com>
2016-07-02 18:32:07 -0700

    Move firebase creds to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,10 +2,10 @@ import firebase from 'firebase'
 
 try {
   var config = {
-    apiKey: "AIzaSyCc6ZBf-4X5CMTQURnm5__y3mFf40eBaCY",
-    authDomain: "onyx-todo-app.firebaseapp.com",
-    databaseURL: "https://onyx-todo-app.firebaseio.com",
-    storageBucket: "onyx-todo-app.appspot.com",
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
   };
   firebase.initializeApp(config)
 } catch (e) {
